9ec6e7c43c29de0129bebf4ec8219d720335873c
Louis Côté <2131920@etu.cegepjonquiere.ca>
2023-04-20 14:56:17 -0400

    FiniTest

---
+++ bibli/bibli/calculateur.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace bibli
+{
+    public class Calculateur
+    {
+
+        public int Adition(int entier1, int entier2)
+        {
+            return entier1 + entier2;
+        }
+
+        public int Soustraction(int nbASoustraire, int nbQuiSoustrait)
+        {
+            return nbASoustraire - nbQuiSoustrait;
+        }
+
+
+        public double Diviser(int nbDiviser, int nbQuiDivise)
+        {
+            try
+            {
+                double resultat = nbDiviser / nbQuiDivise;
+                return resultat;
+            }
+            catch (DivideByZeroException ex)
+            {
+                return nbDiviser;
+            }
+
+        }
+
+        public double Multiplier(int nbMultiplier, int nbQuiMultipie)
+        {
+            double resultat = nbMultiplier * nbQuiMultipie;
+            return ++resultat;
+        }
+
+
+        public double Log(double a, double b)
+        {
+            return Math.Log(a, b);
+        }
+        
+    }
+}
---
+++ bibli/bibliTests/CalculateurTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using bibli;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bibli.Tests
+{
+    [TestClass()]
+    public class CalculateurTests
+    {
+        [TestMethod()]
+        public void AditionTest()
+        {
+            Calculateur cal = new Calculateur();
+            if (cal.Adition(5, 4) != 9)
+                Assert.Fail();
+           
+        }
+        [TestMethod()]
+        public void Soustraction()
+        {
+            Calculateur cal = new Calculateur();
+            if (cal.Soustraction(22, 10) != 12)
+                Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Diviser()
+        {
+            Calculateur cal = new Calculateur();
+            if (cal.Diviser(50, 5) != 10)
+                Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Multiplier()
+        {
+            Calculateur cal = new Calculateur();
+            if (cal.Multiplier(10, 5) != 50)
+                Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Log()
+        {
+            Calculateur cal = new Calculateur();
+            if (cal.Log(100, 10) != 2)
+                Assert.Fail();
+        }
+    }
+}
